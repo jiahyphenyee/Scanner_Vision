fa86fa6619ef57a093c904b9add9ea05a2013314
Jia Yee <jiayee_koe@mymail.sutd.edu.sg>
2020-10-07 15:22:43 +0800

    configure capture session

--- ScannerTestVision11/ViewController.swift
+++ ScannerTestVision11/ViewController.swift
@@ -7,12 +7,85 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    // MARK:- Private Vars
+    
+    private let captureSession = AVCaptureSession()
+    private var photoOutput = AVCapturePhotoOutput()
+    private let videoDataOutput = AVCaptureVideoDataOutput()
+    // preview the camera feed
+    private lazy var previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+    
+    // MARK:- Private Functions
+    
+    private func setCameraInput() {
+        guard let device = AVCaptureDevice.DiscoverySession(
+            deviceTypes: [.builtInDualCamera, .builtInWideAngleCamera, .builtInTrueDepthCamera],
+            mediaType: .video,
+            position: .back).devices.first else {
+                fatalError("No back camera device found.")
+        }
+        
+        let cameraInput = try! AVCaptureDeviceInput(device: device)
+        self.captureSession.addInput(cameraInput)
+    }
+    
+    private func showCameraFeed(){
+        self.previewLayer.videoGravity = .resizeAspectFill
+        self.view.layer.addSublayer(self.previewLayer)
+        self.previewLayer.frame = self.view.frame
+    }
+    
+    private func setCameraOutput() {
+        self.videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
+        self.videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        self.videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
+        
+        self.captureSession.addOutput(self.videoDataOutput)
+        self.captureSession.addOutput(self.photoOutput)
+        
+        guard let connection = self.videoDataOutput.connections.first ,
+            connection.isVideoOrientationSupported else { return }
+        connection.videoOrientation = .portrait
+        
+    }
 
+    // MARK:- Override Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.setCameraInput()
+        self.showCameraFeed()
+        self.setCameraOutput()
+    }
+    
+    // resize frame of the previewLayer to the real bounds of our view
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.previewLayer.frame = self.view.bounds
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // start session
+        self.videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera_frame_processing_queue"))
+        self.captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // session stopped
+        self.videoDataOutput.setSampleBufferDelegate(nil, queue: nil)
+        self.captureSession.stopRunning()
+    }
+    
+    // MARK:- Functions
+    
+    func captureOutput(_ output: AVCaptureOutput,didOutput sampleBuffer: CMSampleBuffer,from connection: AVCaptureConnection) {
+      // what we will do here ? 🤨
     }
 
 

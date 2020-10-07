36e48f2d3e32443004f4c75c12fe7b4b1f7b87c7
Jia Yee <jiayee_koe@mymail.sutd.edu.sg>
2020-10-07 14:03:38 +0800

    Initial Commit

---
+++ ScannerTestVision11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScannerTestVision11
+//
+//  Created by Koe Jia-Yee on 7/10/20.
+//  Copyright © 2020 Koe Jia-Yee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
